60f8f8956757821e66b95372349749e29415cb76
Cristian Vuolo <cristianvuolo@gmail.com>
2020-06-06 09:48:46 -0300

    add filters and details queries

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,10 +14,39 @@ class UserController extends Controller
      */
     public function index()
     {
-        return User::with('items')->get()->map(function ($q) {
+        $users = User::with('items');
+
+        if (request('uf')) {
+            $users = $users->where('uf', request('uf'));
+        }
+        if (request('city')) {
+            $users = $users->where('city', request('city'));
+        }
+
+        if (request('items')) {
+            $users = $users->has('items', '>=', 1)->whereHas('items', function ($query) {
+                foreach (explode(',', request('items')) as $item_id) {
+                    $query->orWhere('item_id', $item_id);
+                }
+            });
+        }
+        return $users->get()->map(function ($q) {
             $q->image_url = $q->getFirstMediaUrl('image');
             return $q;
         });
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param User $user
+     * @return User
+     */
+    public function show(User $user)
+    {
+        $user->image_url = $user->getFirstMediaUrl('image');
+        $user->items_ids = implode(',', $user->items()->pluck('item_id')->toArray());
+        return $user;
     }
 
     /**
@@ -30,8 +59,12 @@ class UserController extends Controller
     {
         $user = User::create($request->except('image'));
 
-        if($request->image) {
+        if ($request->image) {
             $user->addMediaFromRequest('image')->toMediaCollection('image');
+        }
+
+        if ($request->items) {
+            $user->items()->sync(explode(',', $request->items));
         }
 
         return $user;
